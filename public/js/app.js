43e026faca1d9460d7abf172c9f500cf6ca06188
vxd447 <vdua447@gmail.com>
2020-01-16 20:33:47 +0530

    Remove unnecassary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,3 @@
-console.log('Using Static Files')
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data);
-//     })
-// })
-
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#messageOne')
